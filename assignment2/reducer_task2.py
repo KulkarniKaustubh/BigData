77b7dd7e9d9cc52b289f14bd043ab8f101c6d501
sriram1999s <sriramsub7@gmail.com>
2020-09-22 22:23:33 +0530

    edited reducer2

--- assignment2/reducer_task2.py
+++ assignment2/reducer_task2.py
@@ -7,5 +7,5 @@ for line in sys.stdin:
 	k,v=line.split(',') # v will contain the sum of contributions of every node that links to k
 	k=k.strip("\'")
 	add=0.15+(0.85*float(v)) # calculating new rank
-	add=float("{:.5f}".format(add))
+	add="{:.5f}".format(add)
 	print(k,add,sep=',')
