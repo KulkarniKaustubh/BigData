6686c26ce4176cc1501ac9c900a1ac7d85d5aac8
sriram1999s <sriramsub7@gmail.com>
2020-09-30 00:00:16 +0530

    edited reduce2

--- assignment2/reduce2.py
+++ assignment2/reduce2.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
 import sys
-
+current=None
+add=None
 for line in sys.stdin:
 	line = line.strip()
 	
 	k,v=line.split(',') # v will contain the sum of contributions of every node that links to k
 	k=k.strip("\'")
-	add=0.15+(0.85*float(v)) # calculating new rank
+	if current==k:
+		add+=float(v)
+	else:
+		
+		if current!=None:
+			add=0.15+(0.85*add) # calculating new rank
+			add="{:.5f}".format(add)
+			print(current,add,sep=',')
+		current=k
+		add=float(v)
+if current==k:
+	add=0.15+(0.85*add)
 	add="{:.5f}".format(add)
-	print(k,add,sep=',')
+	print(current,add,sep=',')
