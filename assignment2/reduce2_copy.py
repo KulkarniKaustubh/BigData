daca37184c4bf12c2b3a7e0b62f3a16de381ed20
KulkarniKaustubh <kmkapril15@gmail.com>
2020-10-02 13:34:02 +0530

    A changed copy of reduce2.py

---
+++ assignment2/reduce2_copy.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import sys
+
+prev_k = None
+add = 0
+
+for line in sys.stdin:
+	line = line.strip()
+
+	k, v = line.split(',') # v will contain the sum of contributions of every node that links to k
+	k = k.strip("'")
+
+	if prev_k and prev_k != k:
+		new="{0:.5f}".format(0.15+(0.85*add))
+		print(f'{prev_k},{new}')
+		add = 0
+
+	prev_k = k
+	add += float(v)
+
+if prev_k:
+	new="{0:.5f}".format(0.15+(0.85*add))
+	print(f'{prev_k},{new}')
