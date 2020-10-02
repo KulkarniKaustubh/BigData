613e571212787e5fe4a40065c1ae5a1f29b4272b
pvn-leo <54072494+pvn-leo@users.noreply.github.com>
2020-10-02 16:00:44 +0530

    Update copy.sh

--- assignment2/tempmr/copy.sh
+++ assignment2/tempmr/copy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cp map1.py BD_0051_0157_0655_1956_mappper_t1.py
-cp map2.py BD_0051_0157_0655_1956_mappper_t2.py
+cp map1.py BD_0051_0157_0655_1956_mapper_t1.py
+cp map2.py BD_0051_0157_0655_1956_mapper_t2.py
 cp reduce1.py BD_0051_0157_0655_1956_reducer_t1.py
 cp reduce2.py BD_0051_0157_0655_1956_reducer_t2.py
