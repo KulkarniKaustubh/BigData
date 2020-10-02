24556e7e643475854077265431450ccde76f3988
KulkarniKaustubh <kmkapril15@gmail.com>
2020-10-02 11:46:57 +0530

    A copy of reducer1.py

---
+++ assignment2/reduce1_copy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+
+path = ''
+if sys.argv[1]:
+	path = sys.argv[1]  # will hold the path to the file v
+
+adj_list = []
+v_file = open(path, "a")
+prev_from_node = None
+
+for line in sys.stdin:
+	line = line.strip()
+	from_node, to_node = line.split('\t')
+
+	if prev_from_node != from_node:
+		print(f'{from_node}\t{adj_list}')
+		v_file.write(f'{from_node}, 1\n')
+		adj_list = []
+
+	prev_from_node = from_node
+	adj_list.append(to_node)
+
+print(f'{prev_from_node}\t{adj_list}')
+v_file.write(f'{prev_from_node}, 1\n')
