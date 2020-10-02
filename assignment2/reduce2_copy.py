0e4ebd67b87b7d2e5428eef3b98a2ade3b99ab47
KulkarniKaustubh <kmkapril15@gmail.com>
2020-10-03 00:37:14 +0530

    Updated reduce2_copy.py

--- assignment2/reduce2_copy.py
+++ assignment2/reduce2_copy.py
@@ -8,7 +8,7 @@ for line in sys.stdin:
 	line = line.strip()
 
 	k, v = line.split(',') # v will contain the sum of contributions of every node that links to k
-	k = k.strip("'")
+	#k = k.strip("'")
 
 	if prev_k and prev_k != k:
 		new="{0:.5f}".format(0.15+(0.85*add))
