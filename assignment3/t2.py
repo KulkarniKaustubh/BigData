952c5312cf44703adc3025a8a6f8a7ebf867c024
sriram1999s <sriramsub7@gmail.com>
2020-10-28 20:06:09 +0530

    task2-done

--- assignment3/t2.py
+++ assignment3/t2.py
@@ -6,7 +6,7 @@ if sys.argv[1]:
 	word=sys.argv[1]
 	
 if(sys.argv[2]):
-	k = sys.argv[2]
+	k = int(sys.argv[2])
 	
 path=''
 if sys.argv[3]:
@@ -14,7 +14,7 @@ if sys.argv[3]:
 
 sc = SparkContext('local',"task2")
 
-rdd = sc.textFile("shape_stat.csv")
+rdd = sc.textFile(path)
 mappedRdd = rdd.map(lambda x: x.split(',')) #to send joined dataset here
 
 columns={}
@@ -31,4 +31,6 @@ un_recRdd_k = un_recRdd.filter(lambda x: int(x[columns['Total_Strokes']]) < k) #
 ctry = un_recRdd_k.map(lambda x: (x[columns['countrycode']], 1)) # [(CA, 1), (US, 1), (CA, 1)]
 count_ctry = ctry.reduceByKey(lambda x,y : (x+y) ).sortByKey()  # [(CA, 2), (US, 1)]
 
-count_ctry.collect()
+#print(count_ctry.collect())
+for item in count_ctry.collect():
+	print(f"{item[0]},{item[1]}")
