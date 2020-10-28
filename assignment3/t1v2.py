e3448e2684358af6322ea7cb6a2666335bd4c44c
sriram1999s <sriramsub7@gmail.com>
2020-10-28 19:05:45 +0530

    version2.0

--- assignment3/t1v2.py
+++ assignment3/t1v2.py
@@ -11,31 +11,31 @@ if sys.argv[2]:
 fil=path
 sc=SparkContext('local',"task1")
 
-rdd=sc.textFile(path)
+rdd=sc.textFile(path) # stores it as rdd automatically
 
-mappedRdd=rdd.map(lambda x: x.split(','))
+mappedRdd=rdd.map(lambda x: x.split(',')) # stores it as 'list of lists' rdd
 #print(mappedRdd.take(5))
 
 
 columns={}
 ind=0
-for i in mappedRdd.first():
+for i in mappedRdd.first(): # stores indices of each column
 	columns[i]=ind
 	ind+=1
 #print(columns)
 
-wordRdd=mappedRdd.filter(lambda x: x[columns['word']]==word)
+wordRdd=mappedRdd.filter(lambda x: x[columns['word']]==word) #gets subset of data where the word attribute is the same as that provided as argument
 
-recRdd=wordRdd.filter(lambda x: x[columns['recognized']]=='True')
+recRdd=wordRdd.filter(lambda x: x[columns['recognized']]=='True') #filters out the recognized drawings
 
 #print(recRdd.take(5))
 
-unrecRdd=wordRdd.filter(lambda x: x[columns['recognized']]=='False')
+unrecRdd=wordRdd.filter(lambda x: x[columns['recognized']]=='False') #filters out the unrecognized drawings
 
 #print(unrecRdd.take(5))
 
-avg_strokes_recRdd=recRdd.map(lambda x : int(x[columns['Total_Strokes']])).mean()
-avg_strokes_unrecRdd=unrecRdd.map(lambda x : int(x[columns['Total_Strokes']])).mean()
+avg_strokes_recRdd=recRdd.map(lambda x : int(x[columns['Total_Strokes']])).mean() #calculates the avg strokes for recognized == True
+avg_strokes_unrecRdd=unrecRdd.map(lambda x : int(x[columns['Total_Strokes']])).mean() #calculates the avg strokes for recognized == False
 print(avg_strokes_recRdd)
 print(avg_strokes_unrecRdd)
 
