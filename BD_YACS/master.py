a75f4a5f3b262c4d9e75b17d9cf0166d3687693a
pvn-leo <pavan.leo36dna@gmail.com>
2020-11-11 12:08:47 +0530

    shotened code - port 5001 on master.py

--- BD_YACS/master.py
+++ BD_YACS/master.py
@@ -132,96 +132,62 @@ with socket(AF_INET, SOCK_STREAM) as s:
 
 
 '''
-
 def listen_updates():
+	#opening this will make port 5001 active and recieve updates from worker.py
+	update = socket(AF_INET, SOCK_STREAM) #init a TCP socket
+	update.bind(('', 5001))
+	update.listen()
+	print("Master ready to recieve task updates from worker.py")
 
-    #opening this will make port 5001 active and recieve updates from worker.py
-    update = socket(AF_INET, SOCK_STREAM) #init a TCP socket
-    update.bind(('', 5001))
-    update.listen()
-    print("Master ready to recieve task updates from worker.py")
+	while(true):
 
-    while(true):
+		connectionSocket, addr = update.accept()
+		message = connectionSocket.recv(2048) # recieve max of 2048 bytes
+		mssg = json.loads(message)
 
-        connectionSocket, addr = update.accept()
-        message = connectionSocket.recv(2048) # recieve max of 2048 bytes
-        mssg = json.loads(message)
+		# taking in the necessary values inorder to increase the slot count and to check if a job has finished executing.
+		task_id = mssg['task_id']
+		worker_id = mssg['worker_id']
 
-        # taking in the necessary values inorder to increase the slot count and to check if a job has finished executing.
+		job_id = task_id.split('_')[0]
 
-        task_id = mssg['task_id']
-        worker_id = mssg['worker_id']
+		if '_M' in task_id: # The task that got completed is a map task
 
-        job_id = task_id.split('_')[0]
-        
+			for job in j:            
+				if(job.jobid == job_id): # Finding the parent job of the map task                    
+					for m_task in job.map_tasks:                        
+						if m_task.taskid == task_id: 
+							m_task.done = True # Updating the map task's done to True
+							job.map_tasks_done += 1 # Incrementing the number of map tasks completed for that particular job
+							break
 
-        if '_M' in task_id: # The task that got completed is a map task
+				break
 
-            for job in j:
-            
-                if(job.jobid == job_id): # Finding the parent job of the map task
-                    
-                    for m_task in job.map_tasks:
-                        
-                        if m_task.taskid == task_id: 
-                            m_task.done = True # Updating the map task's done to True
-                            job.map_tasks_done += 1 # Incrementing the number of map tasks completed for that particular job
-                            break
+		else:
 
-                break
-
-        else:
-
-            for job in j:
-            
-                if(job.jobid == job_id): # Finding the parent job of the reduce task
-                    
-                    for r_task in job.reduce_tasks:
-                        
-                        if r_task.taskid == task_id:
-                            r_task.done = True # Updating the reduce task's done to True
-                            job.reduce_tasks_done += 1 # Incrementing the number of reduce tasks completed for that particular job
-                            break
-
-                break
+			for job in j:            
+				if(job.jobid == job_id): # Finding the parent job of the reduce task                    
+					for r_task in job.reduce_tasks:                        
+						if r_task.taskid == task_id:
+							r_task.done = True # Updating the reduce task's done to True
+							job.reduce_tasks_done += 1 # Incrementing the number of reduce tasks completed for that particular job
+							break
+				break
 
 
-        for worker in workers:
-            if worker.id == worker_id:
-                worker.slot += 1 # Since the task got completed, we are deallocating the slot that had been used by the task.
+		for worker in workers:
+			if worker.id == worker_id:
+				worker.slot += 1 # Since the task got completed, we are deallocating the slot that had been used by the task.
 
 
-        # To check if the entire job is done
-        flag = True
+		# To check if the entire job is done
+		for job in j:
 
-        for job in j:
+			if(job.jobid == job_id): #searching for job_id
+				if( (len(job.map_tasks) == job.map_tasks_done) and (len(job.reduce_tasks) == job.reduce_tasks_done)):
+					job.job_done = True # Updating the job's done to True
+					print('Job ', job_id, ' was processed successfully', end = '\n')
 
-            if(job.jobid == job_id):
-
-                if job.map_tasks_done != len(job.map_tasks): # If the map task counts don't match, the job isn't done yet.
-                    flag = False
-                    break
-                
-                if flag == True:
-
-                    if job.reduce_tasks_done != len(job.reduce_tasks): # If the reduce task counts don't match, the job isn't done yet.
-                        flag = False
-                        break
-
-                break
-
-        
-        if Flag == True: # This implies all the tasks of the job are complete
-
-            for job in j:
-
-                if(job.jobid == job_id):
-                    job.job_done = True # Updating the job's done to True
-                    print('Job ', job_id, ' was processed successfully', end = '\n')
-                    break
-
+				break
 update.close()
-
-
-
 '''
