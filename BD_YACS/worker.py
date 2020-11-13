71ab974fb5045816fdf14f73455386ce7f658bca
sriram1999s <sriramsub7@gmail.com>
2020-11-13 11:33:23 +0530

    updated worker

--- BD_YACS/worker.py
+++ BD_YACS/worker.py
@@ -2,6 +2,7 @@ import json
 import sys
 from socket import * 
 import time
+import threading
 
 try:
 	port = int(sys.argv[1])
@@ -33,8 +34,22 @@ exe_pool = []
 task_in_socket = socket(AF_INET,SOCK_STREAM) #init a TCP socket
 task_in_socket.bind(('',port)) #listen on port 5000, from requests.py
 task_in_socket.listen(3)
+
+"""
+All semaphores are defined here
+"""
+lock=threading.Semaphore(1)
+
+"""
+Semaphore definitions end
+"""
+
 print("Worker ready to recieve tasks from master.py")
 
+
+"""
+listener code
+"""
 def task_in():
 	while(1):
 		connectionSocket, addr = task_in_socket.accept() 
@@ -43,20 +58,67 @@ def task_in():
 		mssg = json.loads(message)
 		#this will apend the task to exe pool
 		sample_test = task(mssg['taskid'], mssg['duration'], mssg['jobid'], mssg['workerid'])
+		
+		lock.acquire()
 		exe_pool.append(sample_test)
-		sample_test.print()
+		lock.release()
+		
+		#sample_test.print()
 		#this is a dummy line to mimic completion of the task
-		sample_test.done = True
-		sample_test.print()
+		"""sample_test.done = True
+		sample_test.print()"""
+		#task_exec()
 		#need to add task to exe pool and do the execution
 
-def task_out():#take a task as input to send it through .send
+"""listener code ends"""
+
+"""updater code"""
+def task_out(send_task):#take a task as input to send it through .send
+	'''send_task.done=True
+	print("here")						    ### use this as of now
+	send_task.print()'''
 	with socket(AF_INET, SOCK_STREAM) as s:
 		s.connect(("localhost", 5001))
-		send_task = task.to_json(jobs[0].map_tasks[0])
+		print("###---sending update to master---###")
+		#send_task = task.to_json(jobs[0].map_tasks[0])      ### won't work as of now ...till master is ready to accept updates.
 		message=json.dumps(send_task)
 		s.send(message.encode())
+		print('done...')
+"""updater code ends"""
 
-task_in()
+"""executor code"""
+def task_exec():
+	while 1:
+		for task in exe_pool:
+			print('-'*60)
+			print(f"executing task with id {task.taskid}")
+			
+			lock.acquire()
+			task.duration-=1
+			task.print()
+			if task.duration==0:
+				print(f"Task {task.taskid} has finished execution")
+				#task_out(send_task)
+				updater_thread=threading.Thread(target=task_out,args=(task,))
+				updater_thread.start()
+				
+				updater_thread.join()
+				exe_pool.remove(task)
+			lock.release()
+			
+			print("")
+			print('-'*60)
+			
+"""executor code ends"""
 
-time.sleep(10)
+''' Running worker'''
+listening_thread=threading.Thread(target=task_in)
+executing_thread=threading.Thread(target=task_exec)
+#task_in()
+listening_thread.start()
+
+executing_thread.start()
+#time.sleep(10)
+
+
+
